b8cc4a4bdf3dc9495d770519580debbea78702a0
TumisangF <62010042+TumisangF@users.noreply.github.com>
2021-02-09 10:25:08 +0200

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,12 @@
+array = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings (string, array)
+    matches = {}
+    dictionary.each do |word|
+        matches[word] = string.downcase.scan(/(?=#{word})/).count if string.downcase.include?(word)
+    end
+    matches
+end
+
+puts substrings("Below", array)
+puts substrings("Howdy partner, sit down! How's it going?", array)
